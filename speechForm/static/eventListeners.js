89f1543bc4f6293b73960b6ee33719697ecea8b5
Aekus Trehan <aekus.trehan@gmail.com>
2023-02-11 19:53:33 -0500

    Added other question pages with routing that should work

--- speechForm/static/eventListeners.js
+++ speechForm/static/eventListeners.js
@@ -1,18 +1,25 @@
+let page;
+
 function insertvalue(text) {
   // console.log(text)
-  resulting = text['result']
+  resulting = text["result"];
   // console.log(resulting)
   document.getElementById("user-input").value = resulting;
   document.getElementById("submit_button").click();
 }
 
 function handleSubmitButton() {
-  fetch('/form')
+  page++;
+  fetch("/form")
     .then((response) => response.json())
     .then((data) => insertvalue(data));
+  location.replace("127.0.0.1:5000/index" + page);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   // This is called after the browser has loaded the web page
-  document.getElementById("submit_button").addEventListener("click", handleSubmitButton());
+  page = 0;
+  document
+    .getElementById("submit_button")
+    .addEventListener("click", handleSubmitButton());
 });
